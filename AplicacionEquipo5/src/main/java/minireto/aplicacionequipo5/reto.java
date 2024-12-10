67104393c0d6e0643cfa9ae1fb0060dfdbfccf8d
mariocuee38 <mario.cuevasgarcia@iesmiguelherrero.com>
2024-12-10 19:26:18 +0100

    Reto y enums
    clase reto con sus atributos y enums necesarios

---
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/reto.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minireto.aplicacionequipo5;
+import java.util.Date;
+/**
+ *
+ * @author Mario Cuevas
+ */
+public class reto {
+    private String nomReto;
+    private String nomUsuario;
+    private Date fechaHora;
+    private Actividades tipoActividad;
+    private int distancia;
+    private int desnivelNeg;
+    private int denivelPos;
+    private String periodo;
+    private Dificultad dificultad;
+    private String comentario;
+}
