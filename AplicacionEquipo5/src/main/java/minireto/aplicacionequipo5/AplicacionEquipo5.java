857bb11e54862ec4ed5d78f08ed7c6018fdbdc84
paco11111 <raul.buenagagarcia@iesmiguelherrero.com>
2024-12-10 20:12:58 +0100

    perfil

--- AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/AplicacionEquipo5.java
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/AplicacionEquipo5.java
@@ -12,5 +12,6 @@ public class AplicacionEquipo5 {
 
     public static void main(String[] args) {
         System.out.println("Hey");
+                
     }
 }
---
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/Perfil.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minireto.aplicacionequipo5;
+
+/**
+ *
+ * @author DAM105
+ */
+public class Perfil {
+    
+    private String nombre,apellidos,estadisticasGenerales,retosRealizados;
+
+    public Perfil() {
+    }
+  
+    public void usuarioEstadisticas(){
+    
+}
+}
