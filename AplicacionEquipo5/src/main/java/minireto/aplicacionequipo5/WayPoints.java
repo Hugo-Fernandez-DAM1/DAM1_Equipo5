67ff45b7ab7dd84ef4f9d5123162278a2634dacb
Hugo-Fernandez-DAM1 <hfernandezc02@educantabria.es>
2024-12-10 20:14:44 +0100

    CClase de WayPoints creada

--- AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/WayPoints.java
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/WayPoints.java
@@ -10,8 +10,97 @@ package minireto.aplicacionequipo5;
  */
 public class WayPoints {
     
-    private Reto[] reto;
+    private int id;
+    private Reto reto;
     private int NumeroWaypoint;
     private int distanciaInicio;
-    
+    private String tipo;
+    private String nombre;
+    private int latitud;
+    private int longitud;
+    private String foto;
+
+    public WayPoints(int id, Reto reto, int NumeroWaypoint, int distanciaInicio, String tipo, String nombre, int latitud, int longitud, String foto) {
+        this.id = id;
+        this.reto = reto;
+        this.NumeroWaypoint = NumeroWaypoint;
+        this.distanciaInicio = distanciaInicio;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.foto = foto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Reto getReto() {
+        return reto;
+    }
+
+    public int getNumeroWaypoint() {
+        return NumeroWaypoint;
+    }
+
+    public int getDistanciaInicio() {
+        return distanciaInicio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getLatitud() {
+        return latitud;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setReto(Reto reto) {
+        this.reto = reto;
+    }
+
+    public void setNumeroWaypoint(int NumeroWaypoint) {
+        this.NumeroWaypoint = NumeroWaypoint;
+    }
+
+    public void setDistanciaInicio(int distanciaInicio) {
+        this.distanciaInicio = distanciaInicio;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setLatitud(int latitud) {
+        this.latitud = latitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
