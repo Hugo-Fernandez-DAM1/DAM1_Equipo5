c7ff0f4cfb6334905671495863faae90f02a00d0
Hugo Fernández Calzado <DAM119@iesmhp.local>
2024-12-10 17:58:22 +0100

    Repositorio

---
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/AplicacionEquipo5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package minireto.aplicacionequipo5;
+
+/**
+ *
+ * @author DAM119
+ */
+public class AplicacionEquipo5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
