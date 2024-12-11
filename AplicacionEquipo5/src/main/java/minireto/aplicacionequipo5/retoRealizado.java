1340621fb8159833dccebd3471fec64475a31945
mariocuee38 <mario.cuevasgarcia@iesmiguelherrero.com>
2024-12-11 16:07:27 +0100

    Revert "Cambio nombre variable"
    This reverts commit 2c9fdbdb839ff5e633fc84d529d972c29b785a3e.

--- AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/retoRealizado.java
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/retoRealizado.java
@@ -13,13 +13,13 @@ import java.time.LocalDateTime;
 public class retoRealizado {
     private LocalDateTime fInicio;
     private LocalDateTime fFinal;
-    private int tiempoEnMovimientoMinutos;
+    private int tiempoEnMovimiento;
     private String comentarios;
 
     public retoRealizado(LocalDateTime fInicio, LocalDateTime fFinal, int tiempoEnMovimiento) {
         this.fInicio = fInicio;
         this.fFinal = fFinal;
-        this.tiempoEnMovimientoMinutos = tiempoEnMovimiento;
+        this.tiempoEnMovimiento = tiempoEnMovimiento;
         
         //AÑADIR EN EL PRINCIPAL PEDIR COMENTARIOS OPCIONALES
     }
