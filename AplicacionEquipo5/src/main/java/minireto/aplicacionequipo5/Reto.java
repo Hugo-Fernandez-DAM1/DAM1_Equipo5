5f39acdf5f078daa52515b7e2756b968ece6a7d2
Mario Cuevas <92816047+mariocuee38@users.noreply.github.com>
2024-12-11 17:27:56 +0100

    Cambios en usuario y una variable de reto
    Ya esta completo el constructor de retos y la funcion del usuario de crear reto

--- AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/Reto.java
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/Reto.java
@@ -18,7 +18,7 @@ public class Reto {
     private int distancia;
     private Dificultad dificultad;
     private int numWP;
-    private WayPoints waypoints[];
+    private WayPoints[] waypoints;
     
     //OPCIONALES
     private int desnivelNeg;
@@ -41,34 +41,7 @@ public class Reto {
         
         this.waypoints=new WayPoints[this.numWP];
     
-
-        
-        /*PASAR AL PRINCIPAL DONDE CONSRUYES EL OBJETO
-        
-        System.out.println("Quieres introducir desniveles? Y/N");
-        char desnivel= t.next().toUpperCase().charAt(0);
-        if (desnivel=='Y'){
-            System.out.println("desnivel positivo:");
-            setDesnivelPos(t.nextInt());
-            System.out.println("desnivel negativo:");
-            setDesnivelNeg(t.nextInt());
-            
-        }
-        
-        System.out.println("Quieres introducir el periodo en el que se realizó? Y/N");
-        char p = t.next().toUpperCase().charAt(0);
-        if (p=='Y'){
-            System.out.println("A continuación introduce el periodo:");
-            setPeriodo(t.nextLine());
-        }
-        
-        System.out.println("Quieres introducir un comentario acerca del reto? Y/N");
-        char comment = t.next().toUpperCase().charAt(0);
-        if (comment=='Y'){
-            System.out.println("A continuacion introduce el comentario");
-            setComentario(t.nextLine());
-        }
-        */
+   
     }
 
     public void setDesnivelNeg(Integer desnivelNeg) {
