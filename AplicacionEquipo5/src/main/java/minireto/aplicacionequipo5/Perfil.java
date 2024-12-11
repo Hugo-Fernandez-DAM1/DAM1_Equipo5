b516c51123ca5baf7fe34702b869a8548993ad18
paco11111 <raul.buenagagarcia@iesmiguelherrero.com>
2024-12-11 15:41:01 +0100

    a

--- AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/Perfil.java
+++ AplicacionEquipo5/src/main/java/minireto/aplicacionequipo5/Perfil.java
@@ -10,12 +10,19 @@ package minireto.aplicacionequipo5;
  */
 public class Perfil {
     
-    private String nombre,apellidos,estadisticasGenerales,retosRealizados;
+    private String nombre,apellidos;
+    private double totalKilometros;
+    private int totalRetosRealizados,totalRetosCreados;
+    private RetoRealizado[] retosRealizado;
 
-    public Perfil() {
+    public Perfil(String nombre, String apellidos, double totalKilometros, int totalRetosRealizados, int totalRetosCreados, RetoRealizado[] retosRealizado) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.totalKilometros = totalKilometros;
+        this.totalRetosRealizados = totalRetosRealizados;
+        this.totalRetosCreados = totalRetosCreados;
+        this.retosRealizado = new RetoRealizado[0];
     }
-  
-    public void usuarioEstadisticas(){
+        //EN EL MAIN SE HARIA EL METODO VER PERFIL DEL USUARIO CON SUS ESTADISTICAS
     
-}
 }
